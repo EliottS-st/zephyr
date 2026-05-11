aa043bcc940cf3f0d1725569c2c84d68b103f4e6
Eliott Speyser <eliott.speyser@st.com>
2026-05-11 17:04:34 +0200

    mdm cmd handler setup + scan wip

--- drivers/wifi/st67w611m1/st67w611m1_drv.c
+++ drivers/wifi/st67w611m1/st67w611m1_drv.c
@@ -6,30 +6,32 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include "modem_context.h"
-#include "zephyr/sys/printk.h" // to be removed
-#include <stdint.h>
 #define DT_DRV_COMPAT st_st67w611m1
 
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(wifi_st67w611m1, CONFIG_WIFI_LOG_LEVEL);
+
 #include <zephyr/kernel.h>
-#include <zephyr/kernel/thread_stack.h>
 #include <zephyr/device.h>
 #include <zephyr/sys/util.h>
-#include <zephyr/logging/log.h>
 #include <zephyr/net/ethernet.h>
 #include <zephyr/net/net_if.h>
 #include <zephyr/net/wifi_mgmt.h>
 
+#include <modem_context.h>
 #include <modem_cmd_handler.h>
 
 #include "st67w611m1_drv.h"
 #include "st67w611m1_spi_poc.h" // TODO POC
 
-LOG_MODULE_REGISTER(wifi_st67w611m1, CONFIG_WIFI_LOG_LEVEL);
-
 #define ST67W611M1_ETH_MTU 1500
 
-K_KERNEL_STACK_DEFINE(st67_workq_stack, CONFIG_ST67W611M1_WORKQ_STACK_SIZE)
+#define BUF_POOL_BUF_COUNT 8
+#define BUF_POOL_BUF_SIZE  256
+
+NET_BUF_POOL_DEFINE(mdm_recv_pool, BUF_POOL_BUF_COUNT, BUF_POOL_BUF_SIZE, 0, NULL);
+
+K_KERNEL_STACK_DEFINE(st67_workq_stack, CONFIG_ST67W611M1_WORKQ_STACK_SIZE);
 
 static struct st67_data driver_data;
 
@@ -40,10 +42,12 @@ static inline int st67_send_at_cmd(struct st67_data *data, const struct modem_cm
 			      &data->sem_cmd_response_wait, timeout);
 }
 
-static inline int st67_write_spi(struct modem_iface *iface, const uint8_t *buf, size_t size) {
-	printk("SPI WRITE CALL\n");
+static inline int st67_write_spi(struct modem_iface *iface, const uint8_t *buf, size_t size)
+{
+	LOG_DBG("SPI WRITE CALL");
+	printk(buf);
+	printk("\n");
 	return 0;
-
 }
 
 static void st67_scan_work(struct k_work *work)
@@ -51,7 +55,7 @@ static void st67_scan_work(struct k_work *work)
 	int ret;
 	struct st67_data *data;
 	data = CONTAINER_OF(work, struct st67_data, scan_work);
-	LOG_INF("st67_scan_work: started");
+	LOG_DBG("st67_scan_work: started");
 
 	static const struct modem_cmd cmds[] = {
 		// MODEM_CMD_DIRECT("+CWLAP:", ); // TODO no direct cmd
@@ -62,28 +66,53 @@ static void st67_scan_work(struct k_work *work)
 	if (ret < 0) {
 		LOG_ERR("Scan failed: err %d", ret);
 	}
-	printk("This is never reached because of the lack of scan done in the array\n");
+
+out:
+	data->scan_cb = NULL;
+	LOG_DBG("scan work end");
 }
 
-static int st67_scan(const struct device *dev, struct wifi_scan_params *params, scan_result_cb_t cb)
+static int st67_scan(const struct device *dev, struct net_if *iface,
+		     struct wifi_scan_params *params, scan_result_cb_t cb)
 {
 	struct st67_data *data = dev->data;
 
 	ARG_UNUSED(params);
-	ARG_UNUSED(cb);
 
-	LOG_INF("st67_scan: submit scan work");
+	/* Block wifi scan overlapping */
+	if (data->scan_cb != NULL) {
+		return -EINPROGRESS;
+	}
+
+	if (!net_if_is_carrier_ok(iface)) {
+		return -EIO;
+	}
+
+	data->scan_cb = cb;
+
+	LOG_DBG("st67_scan: submit scan work");
 
 	k_work_submit_to_queue(&data->workq, &data->scan_work);
 	return 0;
 }
+
+static const struct modem_cmd response_cmds[] = {
+	MODEM_CMD("OK", NULL, 0U, ""),
+	MODEM_CMD("ERROR", NULL, 0U, ""),
+};
+
+static const struct modem_cmd unsol_cmds[] = {
+	MODEM_CMD("CW:CONNECTED", NULL, 0U, ""),
+};
 
 int st67_init(const struct device *dev)
 {
 	int ret;
 	struct st67_data *data = dev->data;
 
-	// spi_protocol_init(); // TODO POC
+	spi_protocol_init(); // TODO POC
+
+	k_sem_init(&data->sem_cmd_response_wait, 0, 1);
 
 	/* Implemented with work items just like esp_at driver to return the API calls quickly */
 	k_work_init(&data->scan_work, st67_scan_work);
@@ -92,9 +121,16 @@ int st67_init(const struct device *dev)
 
 	/* cmd handler */
 	const struct modem_cmd_handler_config cmd_handler_config = {
+		.match_buf = &data->cmd_match_buf[0],
+		.match_buf_len = sizeof(data->cmd_match_buf),
+		.buf_pool = &mdm_recv_pool,
 		.alloc_timeout = K_NO_WAIT,
 		.eol = "\r\n",
 		.user_data = NULL,
+		.response_cmds = response_cmds,
+		.response_cmds_len = sizeof(response_cmds),
+		.unsol_cmds = unsol_cmds,
+		.unsol_cmds_len = sizeof(unsol_cmds),
 	};
 	ret = modem_cmd_handler_init(&data->mctx.cmd_handler, &data->cmd_handler_data,
 				     &cmd_handler_config);
--- drivers/wifi/st67w611m1/st67w611m1_drv.h
+++ drivers/wifi/st67w611m1/st67w611m1_drv.h
@@ -9,24 +9,28 @@
 #ifndef ZEPHYR_DRIVERS_WIFI_ST67W611M1_DRV_H_
 #define ZEPHYR_DRIVERS_WIFI_ST67W611M1_DRV_H_
 
-#include "modem_cmd_handler.h"
 #include <zephyr/kernel.h>
+#include <zephyr/net/wifi_mgmt.h>
+
 #include <modem_context.h>
+#include <modem_cmd_handler.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#define ST67W611M1_MDM_RECV_BUF_SIZE 128
-#define ST67W611M1_MDM_RECV_MAX_BUF 8
-
 #define ST67W611M1_SCAN_TIMEOUT K_SECONDS(10)
+
+#define ST67W611M1_CMD_MATCH_BUF_SIZE 256
 
 struct st67_data {
 	/* modem commands */
 	struct modem_context mctx;
 	struct modem_cmd_handler_data cmd_handler_data;
-	uint8_t cmd_match_buf[ST67W611M1_MDM_RECV_BUF_SIZE];
+	uint8_t cmd_match_buf[ST67W611M1_CMD_MATCH_BUF_SIZE];
+
+	/* wifi */
+	scan_result_cb_t scan_cb;
 
 	/* work queue and items */
 	struct k_work_q workq;
